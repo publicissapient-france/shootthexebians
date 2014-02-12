2413adad2b5ba2bb879c2eaeff31bc7b804847e0
Pierre-Jean Vardanega <pierrejean.vardanega@gmail.com>
2014-02-12 17:16:43 +0100

    Get ranking

---
+++ src/main/java/fr/xebia/devoxxfr/shootthexebians/business/Rank.java
@@ -0,0 +1,30 @@
+package fr.xebia.devoxxfr.shootthexebians.business;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonAutoDetect(
+        fieldVisibility = JsonAutoDetect.Visibility.ANY,
+        getterVisibility = JsonAutoDetect.Visibility.NONE,
+        setterVisibility = JsonAutoDetect.Visibility.NONE
+)
+public class Rank {
+
+    private Long rank;
+    private String player;
+    private Long score;
+
+    @JsonCreator
+    public Rank(@JsonProperty("rank") Long rank,
+                @JsonProperty("player") String player,
+                @JsonProperty("score") Long score) {
+        this.rank = rank;
+        this.player = player;
+        this.score = score;
+    }
+
+    public void setRank(Long rank) {
+        this.rank = rank;
+    }
+}
---
+++ src/main/java/fr/xebia/devoxxfr/shootthexebians/business/RankingResource.java
@@ -0,0 +1,35 @@
+package fr.xebia.devoxxfr.shootthexebians.business;
+
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import org.jongo.MongoCollection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Path("ranking")
+@Component
+@Consumes(APPLICATION_JSON)
+@Produces(APPLICATION_JSON)
+public class RankingResource {
+
+    @Autowired
+    private MongoCollection scoreCollection;
+
+    @GET
+    public List<Rank> buildRanking() {
+        Iterable<Rank> sortedRanks = scoreCollection.find().sort("{score: -1}").as(Rank.class);
+        List<Rank> ranking = new ArrayList<>();
+        Long index = 1L;
+        for (Rank rank : sortedRanks) {
+            rank.setRank(index++);
+            ranking.add(rank);
+        }
+        return ranking;
+    }
+
+}
